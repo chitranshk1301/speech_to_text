a9ac18c3ed082ffda67feca01859f3182c48590f
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-02-03 19:54:13 +0530

    added listening indicator

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
 
 
   return (
-    <div className="h-screen bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
+    <div className="h-full bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
       <div className="mx-10 flex flex-col items-center ">
         <h1 className="text-7xl font-extrabold text-center text-slate-300 mt-28">Speech To Text</h1>
         <p className="text-xl text-center font-bold font-mono underline underline-offset-4 hover:decoration-4 mt-20 text-slate-100"> Press the record button</p>
@@ -70,12 +70,12 @@ function App() {
           </button>
         </div>
       </div>
-      <span>
+      <span className='text-white font-mono ml-20'>
          listening:
          {' '}
          {listening ? 'on' : 'off'}
        </span>
-      <div className="flex flex-row space-x-10 mt-10">
+      <div className="flex flex-row space-x-10 mt-10 pb-20">
         <div className="mx-20 h-80 w-5/12 rounded-xl mt-16 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-80 border border-gray-200">
         <p className="text-white font-extrabold ml-10 mt-7">English:</p>
         <p className='text-white'>
